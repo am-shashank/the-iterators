bccb77b37b8bd0616b322a600c45f00afe9ccce7
am-shashank <am.seshank@gmail.com>
2016-04-09 19:42:45 -0400

    priority queue in the making

--- utils.h
+++ utils.h
@@ -1,3 +1,21 @@
 #include<string>
-
+#include <queue>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+ 
 string getIp();
+
+
+class Queue
+{
+  
+	private:
+  		std::queue<T> queue_;
+  		std::mutex mutex_;
+  		std::condition_variable cond_;
+
+	public:
+				
+ 
+};
